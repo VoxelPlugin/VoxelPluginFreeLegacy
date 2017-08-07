810e8794838d3383d5a41f6ed6d6f91b73fc2e52
Phyronnaz <phyronnaz@gmail.com>
2017-08-07 21:23:55 +0200

    Smooth (not working)
    Former-commit-id: f6e04bdac74f4268577ab3a755dd20cef797f1e2 [formerly c7d0d6ce493657ce5b5700c83f8188d98fc86846]
    Former-commit-id: 48eece005f987eeba706b63db9afa7fe823dd980

--- Source/Procedural/Private/VoxelTools.cpp
+++ Source/Procedural/Private/VoxelTools.cpp
@@ -7,9 +7,15 @@
 
 void UVoxelTools::SetValueSphere(AVoxelWorld* World, FVector Position, float Radius, bool bAdd, bool bQueueUpdate, bool bApplyUpdates, bool bAsync)
 {
+	if (World == NULL)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("World is NULL"));
+		return;
+	}
 	check(World);
 	FIntVector LocalPosition = World->GlobalToLocal(Position);
 	int IntRadius = FMath::CeilToInt(Radius);
+
 	for (int x = -IntRadius; x <= IntRadius; x++)
 	{
 		for (int y = -IntRadius; y <= IntRadius; y++)
@@ -19,17 +25,20 @@ void UVoxelTools::SetValueSphere(AVoxelWorld* World, FVector Position, float Rad
 				FIntVector CurrentPosition = LocalPosition + FIntVector(x, y, z);
 				float Distance = FVector(x, y, z).Size();
 
-				float Alpha = FMath::Clamp(Radius - Distance, -1.f, 1.f);
-
-				int Value = (bAdd ? -1 : 1) * (int)(127 * Alpha);
-
-
-				if ((Value < 0 && bAdd) || (Value >= 0 && !bAdd) || (World->GetValue(CurrentPosition) * Value > 0))
+				if (Distance <= Radius + 1)
 				{
-					World->SetValue(CurrentPosition, Value);
-					if (bQueueUpdate)
+					float Alpha = FMath::Clamp(Radius - Distance, -1.f, 1.f);
+
+					int Value = (bAdd ? -1 : 1) * (int)(127 * Alpha);
+
+
+					if ((Value < 0 && bAdd) || (Value >= 0 && !bAdd) || (World->GetValue(CurrentPosition) * Value > 0))
 					{
-						World->QueueUpdate(CurrentPosition);
+						World->SetValue(CurrentPosition, Value);
+						if (bQueueUpdate)
+						{
+							World->QueueUpdate(CurrentPosition);
+						}
 					}
 				}
 			}
@@ -41,11 +50,17 @@ void UVoxelTools::SetValueSphere(AVoxelWorld* World, FVector Position, float Rad
 	}
 }
 
-void UVoxelTools::SetColorSphere(AVoxelWorld* World, FVector Position, float Radius, FLinearColor Color, bool bQueueUpdate, bool bApplyUpdates, bool bAsync)
+void UVoxelTools::SetColorSphere(AVoxelWorld* World, FVector Position, float Radius, FLinearColor Color, float FadeDistance, bool bQueueUpdate, bool bApplyUpdates, bool bAsync)
 {
+	if (World == NULL)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("World is NULL"));
+		return;
+	}
 	check(World);
 	FIntVector LocalPosition = World->GlobalToLocal(Position);
 	int IntRadius = FMath::CeilToInt(Radius);
+
 	for (int x = -IntRadius; x <= IntRadius; x++)
 	{
 		for (int y = -IntRadius; y <= IntRadius; y++)
@@ -54,7 +69,7 @@ void UVoxelTools::SetColorSphere(AVoxelWorld* World, FVector Position, float Rad
 			{
 				FIntVector CurrentPosition = LocalPosition + FIntVector(x, y, z);
 				float Distance = FVector(x, y, z).Size();
-				float Alpha = FMath::Clamp((Radius - Distance) / Radius, 0.f, 1.f);
+				float Alpha = (FMath::Clamp(Radius - Distance, -1.f, FadeDistance) + 1) / (1 + FadeDistance);
 				FColor CurrentColor = FLinearColor::LerpUsingHSV(World->GetColor(CurrentPosition), Color, Alpha).ToFColor(true);
 
 				World->SetColor(CurrentPosition, CurrentColor);
@@ -71,10 +86,15 @@ void UVoxelTools::SetColorSphere(AVoxelWorld* World, FVector Position, float Rad
 	}
 }
 
-void UVoxelTools::SetValueProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, int Stength, bool bAdd, float Height, bool bQueueUpdate, bool bApplyUpdates, bool bAsync, bool bDebugLines, bool bDebugPoints)
+void UVoxelTools::SetValueProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, int Stength, bool bAdd, float MaxDistance, bool bQueueUpdate, bool bApplyUpdates, bool bAsync, bool bDebugLines, bool bDebugPoints)
 {
+	if (World == NULL)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("World is NULL"));
+		return;
+	}
 	check(World);
-	FVector ToolPosition = Position + Normal * Height;
+	FVector ToolPosition = Position + Normal * MaxDistance / 2;
 
 	// Compute tangent
 	FVector Tangent;
@@ -101,7 +121,7 @@ void UVoxelTools::SetValueProjection(AVoxelWorld* World, FVector Position, FVect
 			{
 				FHitResult Hit;
 				FVector Start = ToolPosition + Tangent * x * Scale + Bitangent * y * Scale;
-				FVector End = Start - Normal * 2 * Height;
+				FVector End = Start - Normal * MaxDistance;
 				if (bDebugLines)
 				{
 					DrawDebugLine(World->GetWorld(), Start, End, FColor::Magenta, false, 1);
@@ -138,10 +158,15 @@ void UVoxelTools::SetValueProjection(AVoxelWorld* World, FVector Position, FVect
 	}
 }
 
-void UVoxelTools::SetColorProjection(AVoxelWorld * World, FVector Position, FVector Normal, float Radius, FLinearColor Color, float Height, bool bQueueUpdate, bool bApplyUpdates, bool bAsync, bool bDebugLines, bool bDebugPoints)
+void UVoxelTools::SetColorProjection(AVoxelWorld * World, FVector Position, FVector Normal, float Radius, FLinearColor Color, float FadeDistance, float MaxDistance, bool bQueueUpdate, bool bApplyUpdates, bool bAsync, bool bDebugLines, bool bDebugPoints)
 {
+	if (World == NULL)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("World is NULL"));
+		return;
+	}
 	check(World);
-	FVector ToolPosition = Position + Normal * Height;
+	FVector ToolPosition = Position + Normal * MaxDistance / 2;
 
 	// Compute tangent
 	FVector Tangent;
@@ -156,7 +181,85 @@ void UVoxelTools::SetColorProjection(AVoxelWorld * World, FVector Position, FVec
 	// Compute bitangent
 	FVector Bitangent = FVector::CrossProduct(Tangent, Normal).GetSafeNormal();
 
-	TArray<FIntVector> Points;
+	TArray<FIntVector> Positions;
+	TArray<FColor> Colors;
+
+	float Scale = World->GetTransform().GetScale3D().Size() / 4;
+
+	int IntRadius = FMath::CeilToInt(Radius);
+	for (int x = -IntRadius; x <= IntRadius; x++)
+	{
+		for (int y = -IntRadius; y <= IntRadius; y++)
+		{
+			FHitResult Hit;
+			FVector Start = ToolPosition + Tangent * x * Scale + Bitangent * y * Scale;
+			FVector End = Start - Normal * MaxDistance;
+			if (bDebugLines)
+			{
+				DrawDebugLine(World->GetWorld(), Start, End, FColor::Magenta, false, 1);
+			}
+
+			if (World->GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_WorldDynamic))
+			{
+				if (bDebugPoints)
+				{
+					DrawDebugPoint(World->GetWorld(), Hit.ImpactPoint, 2, FColor::Red, false, 1);
+				}
+
+				FIntVector CurrentPosition = World->GlobalToLocal(Hit.ImpactPoint);
+				float Distance = FVector2D(x, y).Size();
+				float Alpha = (FMath::Clamp(Radius - Distance, -1.f, FadeDistance) + 1) / (1 + FadeDistance);
+				FColor CurrentColor = FLinearColor::LerpUsingHSV(World->GetColor(CurrentPosition), Color, Alpha).ToFColor(true);
+
+				if (!Positions.Contains(CurrentPosition))
+				{
+					Positions.Add(CurrentPosition);
+					Colors.Add(CurrentColor);
+				}
+			}
+		}
+	}
+	for (int i = 0; i < Positions.Num(); i++)
+	{
+		World->SetColor(Positions[i], Colors[i]);
+
+		if (bQueueUpdate)
+		{
+			World->QueueUpdate(Positions[i]);
+		}
+	}
+	if (bApplyUpdates)
+	{
+		World->ApplyQueuedUpdates(bAsync);
+	}
+}
+
+void UVoxelTools::SmoothValue(AVoxelWorld * World, FVector Position, FVector Normal, float Radius, float Speed, float MaxDistance, bool bQueueUpdate, bool bApplyUpdates, bool bAsync, bool bDebugLines, bool bDebugPoints)
+{
+	if (World == NULL)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("World is NULL"));
+		return;
+	}
+	check(World);
+	FVector ToolPosition = Position + Normal * MaxDistance / 2;
+
+	// Compute tangent
+	FVector Tangent;
+	// N dot T = 0
+	// <=> N.X * T.X + N.Y * T.Y + N.Z * T.Z = 0
+	// <=> T.Z = -1 / N.Z * (N.X * T.X + N.Y * T.Y)
+	Tangent.X = 1;
+	Tangent.Y = 1;
+	Tangent.Z = -1 / Normal.Z * (Normal.X * Tangent.X + Normal.Y * Tangent.Y);
+	Tangent.Normalize();
+
+	// Compute bitangent
+	FVector Bitangent = FVector::CrossProduct(Tangent, Normal).GetSafeNormal();
+
+	TArray<FIntVector> Positions;
+	int MeanValue = 0;
+
 	float Scale = World->GetTransform().GetScale3D().Size() / 4;
 
 	int IntRadius = FMath::CeilToInt(Radius);
@@ -168,29 +271,43 @@ void UVoxelTools::SetColorProjection(AVoxelWorld * World, FVector Position, FVec
 			{
 				FHitResult Hit;
 				FVector Start = ToolPosition + Tangent * x * Scale + Bitangent * y * Scale;
-				FVector End = Start - Normal * 2 * Height;
+				FVector End = Start - Normal * MaxDistance;
 				if (bDebugLines)
 				{
 					DrawDebugLine(World->GetWorld(), Start, End, FColor::Magenta, false, 1);
 				}
+
 				if (World->GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_WorldDynamic))
 				{
 					if (bDebugPoints)
 					{
 						DrawDebugPoint(World->GetWorld(), Hit.ImpactPoint, 2, FColor::Red, false, 1);
 					}
-					Points.AddUnique(World->GlobalToLocal(Hit.ImpactPoint));
+
+					FIntVector CurrentPosition = World->GlobalToLocal(Hit.ImpactPoint);
+
+					if (!Positions.Contains(CurrentPosition))
+					{
+						Positions.Add(CurrentPosition);
+						MeanValue += World->GetValue(CurrentPosition);
+					}
 				}
 			}
 		}
 	}
-	for (FIntVector Point : Points)
+
+	if (Positions.Num() != 0)
 	{
-		World->SetColor(Point, Color.ToFColor(true));
+		MeanValue /= Positions.Num();
+	}
+
+	for (FIntVector CurrentPosition : Positions)
+	{
+		World->SetValue(CurrentPosition, FMath::Lerp(World->GetValue(CurrentPosition), MeanValue, Speed));
 
 		if (bQueueUpdate)
 		{
-			World->QueueUpdate(Point);
+			World->QueueUpdate(CurrentPosition);
 		}
 	}
 	if (bApplyUpdates)
--- Source/Procedural/Public/VoxelTools.h
+++ Source/Procedural/Public/VoxelTools.h
@@ -18,14 +18,18 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "4"))
 		static void SetValueSphere(AVoxelWorld* World, FVector Position, float Radius, bool bAdd, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false);
 
-	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "4"))
-		static void SetColorSphere(AVoxelWorld* World, FVector Position, float Radius, FLinearColor Color, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false);
+	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "5"))
+		static void SetColorSphere(AVoxelWorld* World, FVector Position, float Radius, FLinearColor Color, float FadeDistance = 3, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false);
 
 
 
 	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "6"))
-		static void SetValueProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, int Stength, bool bAdd, float Height = 1000, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false, bool bDebugLines = false, bool bDebugPoints = true);
-	
+		static void SetValueProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, int Stength, bool bAdd, float MaxDistance = 1000, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false, bool bDebugLines = false, bool bDebugPoints = true);
+
+	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "6"))
+		static void SetColorProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, FLinearColor Color, float FadeDistance = 3, float MaxDistance = 1000, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false, bool bDebugLines = false, bool bDebugPoints = true);
+
+	// Does not work
 	UFUNCTION(BlueprintCallable, Category = "Voxel", meta = (AdvancedDisplay = "5"))
-		static void SetColorProjection(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, FLinearColor Color, float Height = 1000, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false, bool bDebugLines = false, bool bDebugPoints = true);
+		static void SmoothValue(AVoxelWorld* World, FVector Position, FVector Normal, float Radius, float Speed = 0.01f, float MaxDistance = 1000, bool bQueueUpdate = true, bool bApplyUpdates = true, bool bAsync = false, bool bDebugLines = false, bool bDebugPoints = true);
 };
