577168184879d2802bfc0295ad216b468b50eda9
Phyronnaz <phyronnaz@gmail.com>
2017-08-29 22:56:26 +0200

    Terrain importers layer info (colors)

--- Plugins/Voxel/Source/Voxel/Private/TerrainImporterWorldGenerator.cpp
+++ Plugins/Voxel/Source/Voxel/Private/TerrainImporterWorldGenerator.cpp
@@ -31,100 +31,6 @@ float UTerrainImporterWorldGenerator::GetDefaultValue_Implementation(FIntVector 
 		int Y = Position.Y - Center.Y + TerrainObject->Width / 2;
 
 		return Position.Z * World->GetTransform().GetScale3D().Z - TerrainObject->Heights[X + TerrainObject->Width * Y];
-		/*
-
-		float Value = 0;
-
-		if (Blur == EBlur::BE_GaussianBlur)
-		{
-
-			if (0 <= Y - 1)
-			{
-				if (0 <= X - 1)
-				{
-					Value += Values[X - 1 + SizeX * (Y - 1)];
-				}
-				Value += 2 * Values[X + SizeX * (Y - 1)];
-				if (X + 1 < SizeX)
-				{
-					Value += Values[X + 1 + SizeX * (Y - 1)];
-				}
-			}
-
-			if (0 <= X - 1)
-			{
-				Value += 2 * Values[X - 1 + SizeX * Y];
-			}
-			Value += 4 * Values[X + SizeX * Y];
-			if (X + 1 < SizeX)
-			{
-				Value += 2 * Values[X + 1 + SizeX * Y];
-			}
-
-			if (Y + 1 < SizeY)
-			{
-				if (0 <= X - 1)
-				{
-					Value += Values[X - 1 + SizeX * (Y + 1)];
-				}
-				Value += 2 * Values[X + SizeX * (Y + 1)];
-				if (X + 1 < SizeX)
-				{
-					Value += Values[X + 1 + SizeX * (Y + 1)];
-				}
-			}
-
-			Value /= 16;
-		}
-		else if (Blur == EBlur::BE_NormalBLur)
-		{
-
-			if (0 <= Y - 1)
-			{
-				if (0 <= X - 1)
-				{
-					Value += Values[X - 1 + SizeX * (Y - 1)];
-				}
-				Value += Values[X + SizeX * (Y - 1)];
-				if (X + 1 < SizeX)
-				{
-					Value += Values[X + 1 + SizeX * (Y - 1)];
-				}
-			}
-
-			if (0 <= X - 1)
-			{
-				Value += Values[X - 1 + SizeX * Y];
-			}
-			Value += Values[X + SizeX * Y];
-			if (X + 1 < SizeX)
-			{
-				Value += Values[X + 1 + SizeX * Y];
-			}
-
-			if (Y + 1 < SizeY)
-			{
-				if (0 <= X - 1)
-				{
-					Value += Values[X - 1 + SizeX * (Y + 1)];
-				}
-				Value += Values[X + SizeX * (Y + 1)];
-				if (X + 1 < SizeX)
-				{
-					Value += Values[X + 1 + SizeX * (Y + 1)];
-				}
-			}
-
-			Value /= 9;
-		}
-		else
-		{
-			Value = Values[X + SizeX * Y];
-		}
-
-		float Alpha = (FMath::Clamp(Value + Position.Z, -1.f, 1.F) + 1) / 2;
-		return FMath::Lerp(InValue, OutValue, Alpha);
-		*/
 	}
 	else
 	{
@@ -134,7 +40,32 @@ float UTerrainImporterWorldGenerator::GetDefaultValue_Implementation(FIntVector 
 
 FColor UTerrainImporterWorldGenerator::GetDefaultColor_Implementation(FIntVector Position)
 {
-	return FColor::Green;
+	check(World);
+
+	if (!TerrainObject)
+	{
+		TerrainObject = Terrain.GetDefaultObject();
+	}
+	if (!TerrainObject)
+	{
+		UE_LOG(VoxelLog, Error, TEXT("Invalid terrain"));
+		return FColor::Black;
+	}
+
+	Position.X -= 4;
+	Position.Y -= 4;
+
+	if (Center.X - TerrainObject->Width / 2 <= Position.X && Position.X < Center.X + TerrainObject->Width / 2 && Center.Y - TerrainObject->Width / 2 <= Position.Y && Position.Y < Center.Y + TerrainObject->Width / 2)
+	{
+		int X = Position.X - Center.X + TerrainObject->Width / 2;
+		int Y = Position.Y - Center.Y + TerrainObject->Width / 2;
+
+		return TerrainObject->Weights[X + TerrainObject->Width * Y];
+	}
+	else
+	{
+		return FColor::Green;
+	}
 }
 
 void UTerrainImporterWorldGenerator::SetVoxelWorld_Implementation(AVoxelWorld* VoxelWorld)
