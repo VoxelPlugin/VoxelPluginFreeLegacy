01496290420e960a08918a0b64fc53b6cf840c87
Phyronnaz <phyronnaz@gmail.com>
2017-09-06 20:22:36 +0200

    Blending of vertex colors only for Blue and Alpha, Red and green are clamped
    Former-commit-id: 783e9058a7b5d43cabaf96a60e0225d4176b5c42 [formerly b01702dcf7ab560e4e975428f97d608742d5e668]
    Former-commit-id: 8448e1a3a67297225780a01cf81d0503c7c811e4

--- Plugins/Voxel/Source/Voxel/Private/TransvoxelTools.h
+++ Plugins/Voxel/Source/Voxel/Private/TransvoxelTools.h
@@ -10,7 +10,9 @@ struct FBoolVector
 	bool Y;
 	bool Z;
 
-	FBoolVector(bool x, bool y, bool z) : X(x), Y(y), Z(z) {}
+	FBoolVector(bool x, bool y, bool z) : X(x), Y(y), Z(z)
+	{
+	}
 };
 
 
@@ -43,7 +45,9 @@ struct VertexProperties2D
 
 struct IRegularVoxel
 {
-	virtual ~IRegularVoxel() {}
+	virtual ~IRegularVoxel()
+	{
+	}
 	virtual float GetValue(int X, int Y, int Z) = 0;
 	virtual FColor GetColor(int X, int Y, int Z) = 0;
 	virtual void SaveVertex(int X, int Y, int Z, short EdgeIndex, int Index, VertexProperties Properties) = 0;
@@ -53,7 +57,9 @@ struct IRegularVoxel
 
 struct ITransitionVoxel
 {
-	virtual ~ITransitionVoxel() {}
+	virtual ~ITransitionVoxel()
+	{
+	}
 	virtual float GetValue2D(int X, int Y) = 0;
 	virtual FColor GetColor2D(int X, int Y) = 0;
 	virtual void SaveVertex2D(int X, int Y, short EdgeIndex, int Index) = 0;
@@ -70,18 +76,31 @@ typedef std::forward_list<VertexProperties2D> Props2D;
 
 namespace TransvoxelTools
 {
-	void RegularPolygonize(IRegularVoxel* Chunk, int X, int Y, int Z, short ValidityMask, Trigs& Triangles, int& TrianglesCount, Verts& Vertices, Props& Properties, Colors& Colors, int& VerticesCount, const int Step);
-	int AddVertex(IRegularVoxel* Chunk, int Step, Verts& Vertices, Props& Properties, Colors& Colors, int& VerticesCount, FVector Vertex, FIntVector ExactPosition, FBoolVector IsExact);
+	void RegularPolygonize(IRegularVoxel* const Chunk, const int X, const int Y, const int Z, const short ValidityMask, Trigs& Triangles, int& TrianglesCount, Verts& Vertices,
+						   Props& Properties, Colors& Colors, int& VerticesCount, const int Step);
 
-	FVector InterpolateX(IRegularVoxel* Chunk, int MinX, int MaxX, int Y, int Z);
-	FVector InterpolateY(IRegularVoxel* Chunk, int X, int MinY, int MaxY, int Z);
-	FVector InterpolateZ(IRegularVoxel* Chunk, int X, int Y, int MinZ, int MaxZ);
+	int AddVertex(IRegularVoxel* const Chunk, const int Step, Verts& Vertices, Props& Properties, Colors& Colors, int& VerticesCount, const FVector Vertex,
+				  const FIntVector ExactPosition, const FBoolVector IsExact, const FColor RGColor);
 
-	void TransitionPolygonize(ITransitionVoxel* Chunk, int X, int Y, short ValidityMask, Trigs& Triangles, int& TrianglesCount, Verts& Vertices, Props2D& Properties, Colors& Colors, int& VerticesCount, const int Step);
-	int AddVertex(ITransitionVoxel* Chunk, bool bIsTranslated, int Step, Verts& Vertices, Props2D& Properties, Colors& Colors, int& VerticesCount, FVector Vertex, FIntVector ExactPosition, FBoolVector IsExact);
 
-	FVector InterpolateX2D(ITransitionVoxel* Chunk, int MinX, int MaxX, int Y);
-	FVector InterpolateY2D(ITransitionVoxel* Chunk, int X, int MinY, int MaxY);
+	FVector InterpolateX(IRegularVoxel* const Chunk, const int MinX, const int MaxX, const int Y, const int Z);
+	FVector InterpolateY(IRegularVoxel* const Chunk, const int X, const int MinY, const int MaxY, const int Z);
+	FVector InterpolateZ(IRegularVoxel* const Chunk, const int X, const int Y, const int MinZ, const int MaxZ);
 
-	FVector GetTranslated(FVector V, FVector N, VertexProperties P, TArray<bool, TFixedAllocator<6>> ChunkHasHigherRes, int Depth);
+
+
+
+
+	void TransitionPolygonize(ITransitionVoxel* const Chunk, const int X, const int Y, const short ValidityMask, Trigs& Triangles, int& TrianglesCount, Verts& Vertices,
+							  Props2D& Properties, Colors& Colors, int& VerticesCount, const int Step);
+
+	int AddVertex(ITransitionVoxel* const Chunk, const bool bIsTranslated, Verts& Vertices, Props2D& Properties, Colors& Colors, int& VerticesCount,
+				  const FVector Vertex, const FIntVector ExactPosition, const FBoolVector IsExact);
+
+
+	FVector InterpolateX2D(ITransitionVoxel* const Chunk, const int MinX, const int MaxX, const int Y);
+	FVector InterpolateY2D(ITransitionVoxel* const Chunk, const int X, const int MinY, const int MaxY);
+
+
+	FVector GetTranslated(const FVector V, const FVector N, const VertexProperties P, const TArray<bool, TFixedAllocator<6>> ChunkHasHigherRes, const int Depth);
 };
