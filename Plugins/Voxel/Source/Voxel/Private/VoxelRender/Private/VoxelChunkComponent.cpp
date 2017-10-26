7119430ad03d7496753ab05601ad9be0d6ff3e47
Phyronnaz <phyronnaz@gmail.com>
2017-10-26 17:30:18 +0200

    Revert "4.18 fix (not working)"
    This reverts commit 7abfd0ca6ea49f22e10057b333a6086db6039846.

--- Plugins/Voxel/Source/Voxel/Private/VoxelRender/Private/VoxelChunkComponent.cpp
+++ Plugins/Voxel/Source/Voxel/Private/VoxelRender/Private/VoxelChunkComponent.cpp
@@ -347,7 +347,7 @@ void UVoxelChunkComponent::ApplyNewFoliage()
 			}
 			else
 			{
-				HierarchicalInstancedStaticMeshComponent->PerInstanceRenderData->UpdateFromPreallocatedData(HierarchicalInstancedStaticMeshComponent, Task.InstanceBuffer, false);
+				HierarchicalInstancedStaticMeshComponent->PerInstanceRenderData->UpdateFromPreallocatedData(HierarchicalInstancedStaticMeshComponent, Task.InstanceBuffer);
 			}
 
 			HierarchicalInstancedStaticMeshComponent->AcceptPrebuiltTree(Task.ClusterTree, Task.OutOcclusionLayerNum);
