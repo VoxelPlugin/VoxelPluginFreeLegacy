0561362e23de2606ddeb039cbb21797f135cab0d
Phyronnaz <phyronnaz@gmail.com>
2017-11-03 20:00:41 +0100

    3D fluids
    Former-commit-id: 233f77e1d29d0986cbbb825418747589b537ced0 [formerly 58b99087cdc483109199c1128693cefa8f4d504f]
    Former-commit-id: 3ebbf17357ee201cbac2d88d197300a1bd0f9c8b

--- Plugins/Voxel/Source/Voxel/Private/Fluids.h
+++ Plugins/Voxel/Source/Voxel/Private/Fluids.h
@@ -3,4 +3,4 @@
 #include "CoreMinimal.h"
 
 
-void FluidStep(const int N, TArray<float> PrevDens, TArray<float> PrevU, TArray<float> PrevV, const float Visc, const float Diff, const float Dt, TArray<float>& OutDens, TArray<float>& OutU, TArray<float>& OutV);
+void FluidStep(const int N, TArray<float>& Dens0, TArray<float>& U0, TArray<float>& V0, TArray<float>& W0, const float Visc, const float Diff, const float Dt, TArray<float>& Dens, TArray<float>& U, TArray<float>& V, TArray<float> W);
