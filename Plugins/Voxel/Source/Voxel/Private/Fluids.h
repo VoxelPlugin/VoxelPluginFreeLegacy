87a3d74e94ca672283569e1e7a9aac845e02700f
Phyronnaz <phyronnaz@gmail.com>
2017-11-10 23:02:49 +0100

    Remove Plugins/Voxel (preparing for submodule)
    Former-commit-id: 69019074fe1428e4dad3bc203a11052ba9645669 [formerly 5ff7d3f48f6d25432766fa5c475acd8dd74d2fb6]
    Former-commit-id: 882b6e6fc8df09523cd652621000c356449015d0

--- Plugins/Voxel/Source/Voxel/Private/Fluids.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "CoreMinimal.h"
-
-
-void FluidStep(const int N, TArray<float>& Dens0, TArray<float>& U0, TArray<float>& V0, TArray<float>& W0, const float Visc, const float Diff, const float Dt, TArray<float>& Dens, TArray<float>& U, TArray<float>& V, TArray<float> W);
