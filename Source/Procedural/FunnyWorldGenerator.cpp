2ae1920193e0f856f1d6ca47cc334fe863f87d74
Phyronnaz <phyronnaz@gmail.com>
2017-11-19 02:16:43 +0100

    Multiplayer shootout + Rocket
    Former-commit-id: f6b3364f46e81350d996c0208ca8c37ad8df849c [formerly 88512b7c641b6b63c23d87eb67230c9b46f71751]
    Former-commit-id: fe713df4a9015d1bcff16fe3f2287341cb3d7d20

--- Source/Procedural/FunnyWorldGenerator.cpp
+++ Source/Procedural/FunnyWorldGenerator.cpp
@@ -11,51 +11,51 @@ UFunnyWorldGenerator::UFunnyWorldGenerator()
 
 }
 
-float UFunnyWorldGenerator::GetDefaultValue(int X, int Y, int Z)
-{
-	if (Z < 0)
-	{
-		return -ValueMultiplier;
-	}
-	else if (SphereLayerHeight <= Z && Z <= SphereLayerHeight + 2 * Radius)
-	{
-		int NX = FMath::Abs(X) % (2 * Radius);
-		int NY = FMath::Abs(Y) % (2 * Radius);
-		int NZ = Z - SphereLayerHeight;
-		NX -= Radius;
-		NY -= Radius;
-		NZ -= Radius;
-
-		return FVector(NX, NY, NZ).Size() - Radius / RadiusDivisor;
-	}
-	else
-	{
-		return ValueMultiplier;
-	}
-}
-
-FVoxelMaterial UFunnyWorldGenerator::GetDefaultMaterial(int X, int Y, int Z)
-{
-	if (Z >= 1)
-	{
-		return BottomMaterial;
-	}
-	else if (Z >= 0)
-	{
-		return TopMaterial;
-	}
-	else if (Z >= -1)
-	{
-		return MiddleMaterial;
-	}
-	else
-	{
-		return BottomMaterial;
-	}
-}
-
-void UFunnyWorldGenerator::SetVoxelWorld(AVoxelWorld* VoxelWorld)
-{
-
-}
+//float UFunnyWorldGenerator::GetDefaultValue(int X, int Y, int Z)
+//{
+//	if (Z < 0)
+//	{
+//		return -ValueMultiplier;
+//	}
+//	else if (SphereLayerHeight <= Z && Z <= SphereLayerHeight + 2 * Radius)
+//	{
+//		int NX = FMath::Abs(X) % (2 * Radius);
+//		int NY = FMath::Abs(Y) % (2 * Radius);
+//		int NZ = Z - SphereLayerHeight;
+//		NX -= Radius;
+//		NY -= Radius;
+//		NZ -= Radius;
+//
+//		return FVector(NX, NY, NZ).Size() - Radius / RadiusDivisor;
+//	}
+//	else
+//	{
+//		return ValueMultiplier;
+//	}
+//}
+//
+//FVoxelMaterial UFunnyWorldGenerator::GetDefaultMaterial(int X, int Y, int Z)
+//{
+//	if (Z >= 1)
+//	{
+//		return BottomMaterial;
+//	}
+//	else if (Z >= 0)
+//	{
+//		return TopMaterial;
+//	}
+//	else if (Z >= -1)
+//	{
+//		return MiddleMaterial;
+//	}
+//	else
+//	{
+//		return BottomMaterial;
+//	}
+//}
+//
+//void UFunnyWorldGenerator::SetVoxelWorld(AVoxelWorld* VoxelWorld)
+//{
+//
+//}
 
