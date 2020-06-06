a39b44bb1a4da120c87f779a1d8f6e6c7148710d
Robert Biehl <rbiehl@irrealdesigns.com>
2020-06-07 00:28:56 +0200

    Update NiagaraDataInterfaceVoxelDataAsset.cpp
    https://gitlab.com/Phyronnaz/VoxelPluginIssues/-/issues/492

--- Source/VoxelNiagara/Private/NiagaraDataInterfaceVoxelDataAsset.cpp
+++ Source/VoxelNiagara/Private/NiagaraDataInterfaceVoxelDataAsset.cpp
@@ -105,6 +105,10 @@ void UNiagaraDataInterfaceVoxelDataAsset::GetFunctions(TArray<FNiagaraFunctionSi
 		Sig.bRequiresContext = false;
 		Sig.Inputs.Add(FNiagaraVariable(FNiagaraTypeDefinition(GetClass()), TEXT("Asset")));
 		Sig.Inputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetVec3Def(), TEXT("Position")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("R")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("G")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("B")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("A")));
 		Sig.SetDescription(VOXEL_LOCTEXT("Color"));
 
 		OutFunctions.Add(Sig);
@@ -116,6 +120,9 @@ void UNiagaraDataInterfaceVoxelDataAsset::GetFunctions(TArray<FNiagaraFunctionSi
 		Sig.bRequiresContext = false;
 		Sig.Inputs.Add(FNiagaraVariable(FNiagaraTypeDefinition(GetClass()), TEXT("Asset")));
 		Sig.Inputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetIntDef(), TEXT("Index")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("X")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("Y")));
+		Sig.Outputs.Add(FNiagaraVariable(FNiagaraTypeDefinition::GetFloatDef(), TEXT("Z")));
 		Sig.SetDescription(VOXEL_LOCTEXT("Position"));
 
 		OutFunctions.Add(Sig);
