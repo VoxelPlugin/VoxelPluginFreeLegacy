0bab9315de0e3eb3c16b00d70c21599d0730a259
Phyronnaz <phyronnaz@gmail.com>
2020-10-18 05:58:58 -0700

    Build 49
    ♻ Remove IOS from whitelist
    
    ✨ Add back the option to not use the palette in the MagicaVox importer
    
    Merge branch 'master' of github.com:Phyronnaz/VoxelPrivate
    
    🐛 Fix landscape collection ensure

--- Source/VoxelEditor/Private/Factories/VoxelDataAssetFactory.cpp
+++ Source/VoxelEditor/Private/Factories/VoxelDataAssetFactory.cpp
@@ -74,6 +74,93 @@ UVoxelDataAssetFromMagicaVoxFactory::UVoxelDataAssetFromMagicaVoxFactory()
 	Formats.Add(TEXT("vox;Magica Voxel Asset"));
 }
 
+bool UVoxelDataAssetFromMagicaVoxFactory::ConfigureProperties()
+{
+	// Load from default
+	bUsePalette = GetDefault<UVoxelDataAssetFromMagicaVoxFactory>()->bUsePalette;
+
+	TSharedRef<SWindow> PickerWindow = SNew(SWindow)
+		.Title(VOXEL_LOCTEXT("Import Magica Vox"))
+		.SizingRule(ESizingRule::Autosized);
+
+	bool bSuccess = false;
+
+	auto OnOkClicked = FOnClicked::CreateLambda([&]() 
+	{
+		bSuccess = true;
+		PickerWindow->RequestDestroyWindow();
+		return FReply::Handled();
+	});
+	auto OnCancelClicked = FOnClicked::CreateLambda([&]() 
+	{
+		bSuccess = false;
+		PickerWindow->RequestDestroyWindow();
+		return FReply::Handled();
+	});
+	
+	FPropertyEditorModule& PropertyEditorModule = FModuleManager::LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
+	FDetailsViewArgs DetailsViewArgs(false, false, false, FDetailsViewArgs::HideNameArea);
+
+	auto DetailsPanel = PropertyEditorModule.CreateDetailView(DetailsViewArgs);
+	DetailsPanel->SetObject(this);
+
+	auto Widget =
+		SNew(SBorder)
+		.Visibility(EVisibility::Visible)
+		.BorderImage(FEditorStyle::GetBrush("Menu.Background"))
+		[
+			SNew(SBox)
+			.Visibility(EVisibility::Visible)
+			.WidthOverride(520.0f)
+			[
+				SNew(SVerticalBox)
+				+SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					DetailsPanel
+				]
+				+SVerticalBox::Slot()
+				.AutoHeight()
+				.HAlign(HAlign_Right)
+				.VAlign(VAlign_Bottom)
+				.Padding(8)
+				[
+					SNew(SUniformGridPanel)
+					.SlotPadding(FEditorStyle::GetMargin("StandardDialog.SlotPadding"))
+					+ SUniformGridPanel::Slot(0, 0)
+					[
+						SNew(SButton)
+						.Text(VOXEL_LOCTEXT("Create"))
+						.HAlign(HAlign_Center)
+						.ContentPadding(FEditorStyle::GetMargin("StandardDialog.ContentPadding"))
+						.OnClicked(OnOkClicked)
+						.ButtonStyle(FEditorStyle::Get(), "FlatButton.Success")
+						.TextStyle(FEditorStyle::Get(), "FlatButton.DefaultTextStyle")
+					]
+					+SUniformGridPanel::Slot(1,0)
+					[
+						SNew(SButton)
+						.Text(VOXEL_LOCTEXT("Cancel"))
+						.HAlign(HAlign_Center)
+						.ContentPadding(FEditorStyle::GetMargin("StandardDialog.ContentPadding"))
+						.OnClicked(OnCancelClicked)
+						.ButtonStyle(FEditorStyle::Get(), "FlatButton.Default")
+						.TextStyle(FEditorStyle::Get(), "FlatButton.DefaultTextStyle")
+					]
+				]
+			]
+		];
+
+	PickerWindow->SetContent(Widget);
+
+	GEditor->EditorAddModalWindow(PickerWindow);
+
+	// Save to default
+	GetMutableDefault<UVoxelDataAssetFromMagicaVoxFactory>()->bUsePalette = bUsePalette;
+
+	return bSuccess;
+}
+
 bool UVoxelDataAssetFromMagicaVoxFactory::FactoryCanImport(const FString& Filename)
 {
 	return FPaths::GetExtension(Filename) == TEXT("vox");
